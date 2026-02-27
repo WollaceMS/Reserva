939155932176dde45b5798c8302080b8d88df1a8
WollaceMS <wollace2099@gmail.com>
2026-02-27 17:55:50 -0300

    env

--- ecommerce/loja/api_mercadopago.py
+++ ecommerce/loja/api_mercadopago.py
@@ -1,8 +1,13 @@
 import mercadopago
 
 
-public_key = 'APP_USR-39efd8bd-1ff6-42bf-994c-301c35f107f2'
-token = 'APP_USR-5103133262076225-121912-d5d06a0edba41971f69e2cf2f5f67b40-3079702380'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+public_key = os.getenv("public_key")
+token = os.getenv("token")
 
 
 def criar_pagamento(itens_pedido, link):
